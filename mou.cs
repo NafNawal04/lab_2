38228ee4eaec5e36f5b3f98a58542e9f293adc17
NafNawal04 <nafisanawal@iut-dhaka.edu>
2023-03-09 00:59:20 +0600

    nafi

---
+++ mou.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _210042130_lab05
+{
+    internal class mou
+    {
+    }
+}
